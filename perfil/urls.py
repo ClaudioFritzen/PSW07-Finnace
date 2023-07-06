417d87d3a4010bb0a0fe4444ec73f1a59695f9a6
Claudio Fritzen <sdfritzen96@gmail.com>
2023-07-06 22:59:59 +0100

    criamos a funcao de criar uma conta

--- perfil/urls.py
+++ perfil/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     
     path('home/', views.home, name='home'),
     path('gerenciar/', views.gerenciar, name='gerenciar'),
+    path('cadastrar_banco/', views.cadastrar_banco, name='cadastrar_banco')
     
 ]
