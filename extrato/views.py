f03cf49ec3e54ecfa38441cfde1bfb1d08b4ab1d
Claudio Fritzen <sdfritzen96@gmail.com>
2023-07-16 23:17:16 +0100

    criamos o banco de dados de valores

--- extrato/views.py
+++ extrato/views.py
@@ -4,7 +4,7 @@ from perfil.models import Categoria, Conta
 
 # Create your views here.
 def novo_valor(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         contas = Conta.objects.all()
-        categorias = Categoria.objects.all()
-        return render(request, 'novo_valor.html', {'categorias':categorias, 'contas':contas})
+        categorias = Categoria.objects.all() 
+        return render(request, 'novo_valor.html', {'contas': contas, 'categorias': categorias})
