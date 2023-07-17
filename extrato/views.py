feb5385fb12f5b72d459440cc99b74ec9bd5b2b7
Claudio Fritzen <sdfritzen96@gmail.com>
2023-07-17 23:10:19 +0100

    estamos salvando as entradas e saidas, porem sem alterar dinamicamente os valores

--- extrato/views.py
+++ extrato/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 from perfil.models import Categoria, Conta
+from django.contrib import messages
+from django.contrib.messages import constants
 
+from extrato.models import Valores
 # Create your views here.
 def novo_valor(request):
     if request.method == "GET":
@@ -11,4 +14,33 @@ def novo_valor(request):
         return render(request, 'novo_valor.html', {'contas': contas, 'categorias': categorias})
     
     elif request.method == "POST":
-        return HttpResponse("Mandou os dados para o banco!")
+        valor = request.POST.get('valor')
+        categoria = request.POST.get('categoria')
+        descricao = request.POST.get('descricao')
+        data = request.POST.get('data')
+        conta = request.POST.get('conta')
+        tipo = request.POST.get('tipo')
+
+        ## criando uma instancia no banco
+        valores = Valores (
+            valor=valor,
+            categoria_id=categoria,
+            data=data,
+            descricao=descricao,
+            conta_id=conta,
+            tipo=tipo
+        )
+
+        # salvando de fato
+        valores.save()
+
+        #mensagens
+        # TODO: fazer as mensagens aparecer de acordo com o tipo saida e entrada
+
+        messages.add_message(request, constants.SUCCESS, 'Entrada/Saída cadastrada com sucesso!')
+
+        
+        return redirect('/extrato/novo_valor' )
+        #return render(request, 'novo_valor.html', {'contas': contas, 'categorias': categorias})
+    
+    
