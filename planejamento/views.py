a0a35b02b57e45dc002ee4f058d17f4dfda6cebf
Claudio Fritzen <sdfritzen96@gmail.com>
2023-07-22 21:53:20 +0100

    criamos as rotas e passamos os valores puxando do banco

--- planejamento/views.py
+++ planejamento/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+
+## importando o banco de dados
+from extrato.models import Valores
+
+from perfil.models import Categoria, Conta
+
 
 # Create your views here.
 
 def definir_planejamento(request):
-    pass
+    categorias = Categoria.objects.all()
+    return HttpResponse(categorias)
+    
