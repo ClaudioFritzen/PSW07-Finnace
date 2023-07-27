59b9a2e1c30d741b0f8b5b7965c8ea1e633b5611
Claudio Fritzen <sdfritzen96@gmail.com>
2023-07-27 22:23:39 +0100

    criamos uma função na models para poder pegar o  total de gastos

--- perfil/models.py
+++ perfil/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 class Categoria(models.Model):
@@ -9,6 +9,23 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.categoria
+    
+    ## muito louco isso
+    def total_gasto(self):
+        from extrato.models import Valores
+
+        valores = Valores.objects.filter(categoria__id = self.id).filter(data__month=datetime.now().month).filter(tipo='S')
+
+
+        # TODO: Chamar a função ja feita em utils.py
+        
+        total_valor = 0
+
+        for valor in valores:
+            total_valor += valor.valor
+    
+
+        return total_valor
 
 class Conta(models.Model):
     banco_choices = (
